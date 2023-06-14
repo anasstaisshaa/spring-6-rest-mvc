f884982e63a9e91126dc462936e4ce63465bb40b
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-06-14 18:56:21 +0300

    add  HTTP GET with Spring MVC List Operation

--- src/main/java/com/anastasiiatkachuk/springframework/spring6restmvc/services/BeerService.java
+++ src/main/java/com/anastasiiatkachuk/springframework/spring6restmvc/services/BeerService.java
@@ -3,8 +3,11 @@ package com.anastasiiatkachuk.springframework.spring6restmvc.services;
 import com.anastasiiatkachuk.springframework.spring6restmvc.model.Beer;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+    List<Beer> listBeers();
+
     Beer getBeerById(UUID id);
 }
