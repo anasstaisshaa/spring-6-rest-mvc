785d8f8c4eb1132904f05723c1036f76e52b61d6
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-06-18 17:40:08 +0300

    add Return Data with Mockito

--- src/test/java/com/anastasiiatkachuk/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/com/anastasiiatkachuk/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -1,6 +1,8 @@
 package com.anastasiiatkachuk.springframework.spring6restmvc.controller;
 
+import com.anastasiiatkachuk.springframework.spring6restmvc.model.Beer;
 import com.anastasiiatkachuk.springframework.spring6restmvc.services.BeerService;
+import com.anastasiiatkachuk.springframework.spring6restmvc.services.BeerServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,6 +13,8 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,10 +28,17 @@ class BeerControllerTest {
     @MockBean
     BeerService beerService;
 
+    BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
+
     @Test
     void getBeerById() throws Exception {
+        Beer testBeer = beerServiceImpl.listBeers().get(0);
+
+        given(beerService.getBeerById(any(UUID.class))).willReturn(testBeer);
+
         mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID())
                         .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 }
