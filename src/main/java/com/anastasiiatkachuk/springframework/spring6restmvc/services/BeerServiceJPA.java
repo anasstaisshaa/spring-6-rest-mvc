ecf4394dcc1ad53766ce0805ff1e95a0a8024634
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-07-17 21:23:26 +0300

    JPA Services

---
+++ src/main/java/com/anastasiiatkachuk/springframework/spring6restmvc/services/BeerServiceJPA.java
@@ -0,0 +1,50 @@
+package com.anastasiiatkachuk.springframework.spring6restmvc.services;
+
+import com.anastasiiatkachuk.springframework.spring6restmvc.mappers.BeerMapper;
+import com.anastasiiatkachuk.springframework.spring6restmvc.model.BeerDTO;
+import com.anastasiiatkachuk.springframework.spring6restmvc.repository.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class BeerServiceJPA implements BeerService {
+    private BeerRepository beerRepository;
+    private final BeerMapper beerMapper;
+
+    @Override
+    public List<BeerDTO> listBeers() {
+        return null;
+    }
+
+    @Override
+    public Optional<BeerDTO> getBeerById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public BeerDTO saveNewBeer(BeerDTO beer) {
+        return null;
+    }
+
+    @Override
+    public void updateBeerById(UUID beerId, BeerDTO beer) {
+
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+
+    }
+
+    @Override
+    public void patchBeerById(UUID beerId, BeerDTO beer) {
+
+    }
+}
